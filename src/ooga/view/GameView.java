060d44abea22f2f33ce936acb1a8d7bafdb27cba
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2021-11-13 18:46:36 -0500

    Build pipeline should now work having fixed errors in signatures

--- src/ooga/view/GameView.java
+++ src/ooga/view/GameView.java
@@ -224,18 +224,18 @@ public class GameView extends Application implements PanelListener {
 //    myViewControlPanel = createViewControlPanel();
   }
 
-  //<editor-fold desc="Create Details Pane and Buttons">
-  //create the JavaFX ane on the bottom of the screen; describes colours for cell states as well as simulation parameters
-  private Node createDetailsPanel() {
-    //TODO: remove details panel entirely
-    myType="GameOfLife";
-    myGridColours = defaultGridColours.getString("GameOfLife").split(",");
-    myGameParameters = new String[1];
-    myGameParameters[0] = "None";
-    HistoryPanel myDetailsPanel = new HistoryPanel(gridDisplayLength, myGridColours, myType, myGameParameters);
-    return myDetailsPanel.createHistoryPanel();
-  }
-  //</editor-fold>
+//  //<editor-fold desc="Create Details Pane and Buttons">
+//  //create the JavaFX ane on the bottom of the screen; describes colours for cell states as well as simulation parameters
+//  private Node createDetailsPanel() {
+//    //TODO: remove details panel entirely
+//    myType="GameOfLife";
+//    myGridColours = defaultGridColours.getString("GameOfLife").split(",");
+//    myGameParameters = new String[1];
+//    myGameParameters[0] = "None";
+//    HistoryPanel myDetailsPanel = new HistoryPanel(gridDisplayLength, myGridColours, myType, myGameParameters);
+//    return myDetailsPanel.createHistoryPanel();
+//  }
+//  //</editor-fold>
 
 
 
