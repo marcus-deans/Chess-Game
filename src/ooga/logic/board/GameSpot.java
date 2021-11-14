9dc85b9fcabbadab257a8bd5acffde0296bbab6f
Jeremy Cross <jsc659@duke.edu>
2021-11-14 13:10:20 -0500

    Updated spot

--- src/ooga/logic/board/GameSpot.java
+++ src/ooga/logic/board/GameSpot.java
@@ -1,5 +1,7 @@
 package ooga.logic.board;
 
+import java.util.Objects;
+
 public class GameSpot implements Spot{
     private Piece piece;
     private Coordinate coordinate;
@@ -50,6 +52,20 @@ public class GameSpot implements Spot{
     {
         this.piece=null;
         this.empty=true;
+    }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameSpot spot = (GameSpot) o;
+        return Objects.equals(piece,spot.piece) && coordinate.getX_pos() == spot.coordinate.getX_pos()
+                && coordinate.getY_pos() == spot.coordinate.getY_pos() && typeOfSpot == spot.typeOfSpot;
+    }
+
+    @Override
+    public int hashCode () {
+        return Objects.hash(piece, coordinate.getX_pos(),coordinate.getY_pos(), typeOfSpot);
     }
 
 
