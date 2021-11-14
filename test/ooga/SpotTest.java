ecb64e9d9543b1a08fddd775b20c643be089ae26
Carter Stonesifer <cbs58@duke.edu>
2021-11-14 18:38:49 -0500

    consolidated controllers, GameSpot test, CSV type Updated again

---
+++ test/ooga/SpotTest.java
@@ -0,0 +1,30 @@
+package ooga;
+
+import ooga.logic.board.Piece;
+import ooga.logic.board.coordinate.GameCoordinate;
+import ooga.logic.board.spot.GameSpot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpotTest {
+
+
+    @Test
+    public void testSpotCoordinates(){
+        Piece myPiece = null;
+        GameCoordinate testCoordinate = new GameCoordinate(1,1);
+        GameSpot myGameSpot = new GameSpot(myPiece,0, 0, 0, true);
+        myGameSpot.setCoordinate(1,1);
+        assertEquals(testCoordinate,myGameSpot.getCoordinate());
+    }
+    @Test
+    public void testSpotType(){
+        Piece myPiece = null;
+        GameSpot myGameSpot = new GameSpot(myPiece,0, 0, 0, true);
+        myPiece = new King(1,2,2);
+        myGameSpot.setTypeOfSpot(myPiece);
+        assertEquals(King,myGameSpot.getTypeOfSpot());
+    }
+
+}
