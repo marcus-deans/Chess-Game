8867109f7a65b09c2595787afe939078e405f890
Jeremy Cross <jsc659@duke.edu>
2021-11-14 12:54:47 -0500

    Basic spot class done, GameBoard and GameCoordinate classes added

---
+++ src/ooga/logic/board/GameBoard.java
@@ -0,0 +1,14 @@
+package ooga.logic.board;
+
+public class GameBoard implements Board{
+    private Spot spot;
+
+    public GameBoard()
+    {
+    }
+    public int[][] getFullBoard()
+    {
+        return
+    }
+
+}
