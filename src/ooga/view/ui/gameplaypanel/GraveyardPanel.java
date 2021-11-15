45901fb128cdb7256b603ca70ece3b41765cd97b
Marcus Deans <marcus.deans@duke.edu>
2021-11-15 16:10:56 -0500

    Added merge request template

--- src/ooga/view/ui/gameplaypanel/GraveyardPanel.java
+++ src/ooga/view/ui/gameplaypanel/GraveyardPanel.java
@@ -1,7 +1,10 @@
 package ooga.view.ui.gameplaypanel;
 
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import ooga.view.ui.SharedUIComponents;
 
 public class GraveyardPanel extends SharedUIComponents {
@@ -13,4 +16,46 @@ public class GraveyardPanel extends SharedUIComponents {
     VBox graveyardPanel = new VBox();
     return graveyardPanel;
   }
+
+  /**
+   * Create the details panel that displays colour legend and parameters of the simulation
+   *
+   * @return the JavaFX HBox that constitutes the details panel
+   */
+  public Node createHistoryPanel() {
+    VBox myHistoryPanel = new VBox();
+    myHistoryPanel.setSpacing(getInt("gameplay_subpanel_spacing"));
+    myHistoryPanel.setId("history-panel");
+
+    Text historyPanelTitle = makeText(getWord("history_panel_title"));
+    ScrollPane historyScrollPane = makeHistoryScrollPane();
+    myHistoryPanel.getChildren().addAll(historyPanelTitle, historyScrollPane);
+
+    myHistoryPanel.setVgrow(historyScrollPane, Priority.ALWAYS);
+
+    return myHistoryPanel;
+  }
+
+  private ScrollPane makeHistoryScrollPane(){
+    ScrollPane newScrollPane = new ScrollPane();
+    newScrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
+    newScrollPane.fitToWidthProperty().set(true);
+
+    newScrollPane.setVmax(getInt("max_history_scrollpane_height"));
+    newScrollPane.setPrefSize(getInt("pref_history_scrollpane_width"), getInt("pref_history_scrollpane_height"));
+    newScrollPane.setContent(myHistoryContent);
+
+//    sp.vvalueProperty().addListener((ObservableValue<? extends Number> ov,
+//        Number old_val, Number new_val) -> {
+//      fileName.setText(imageNames[(new_val.intValue() - 1)/100]);
+//    });
+
+    return newScrollPane;
+  }
+
+  public void addHistory(String action){
+    Text newHistoryEntry = makeText(action);
+    myHistoryContent.getChildren().add(newHistoryEntry);
+  }
+
 }
