04f942adcbf34237e43061288f16d0559fec599f
Carter Stonesifer <cbs58@duke.edu>
2021-11-16 00:47:33 -0500

    Added controller test, going to move to using issues

--- src/ooga/controller/ChessController.java
+++ src/ooga/controller/ChessController.java
@@ -38,15 +38,16 @@ public class ChessController implements Controller {
 
     public ChessController(int width, int height, String background, String filename){
         myGameView = new GameView(width, height, background, filename, this);
-        myGameView.start(new Stage());
+        //TODO: Uncomment the next line when functional
+        //myGameView.start(new Stage());
     }
 
-    public ChessController(int width, int height, String background){
-        new ChessController(width, height, background, "Standard.sim");
-    }
+//    public ChessController(int width, int height, String background){
+//        new ChessController(width, height, background, "Standard.sim");
+//    }
 
 
-    private void setData(File file) throws CsvValidationException, IOException, IncorrectCSVFormatException, IncorrectSimFormatException {
+    void setData(File file) throws CsvValidationException, IOException, IncorrectCSVFormatException, IncorrectSimFormatException {
         File simFile= new File(String.valueOf(file)); //TODO: Set up choosing files
         myData = mySIMParser.readSimFile(simFile);
         File csvFile= new File(myData.get("GameConfiguration"));
@@ -55,24 +56,17 @@ public class ChessController implements Controller {
         BOARDHEIGHT = myCSVParser.getDimensions()[1];
         myBoard = myCSVParser.getInitialStates();
     }
+    public int getHeight(){
+        return BOARDHEIGHT;
+    }
 
     private void initializeGame(){
-
-
 
     }
 
 
     @Override
     public void initializeFromFile(File file) {
-
-
-
-
-
-
-
-
     }
 
     private void selectPiece(int i, int j){
