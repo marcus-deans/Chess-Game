c44b05280e9925a46061462d79a554ce47839619
Marcus Deans <marcus.deans@duke.edu>
2021-11-16 19:41:13 -0500

    Fixing composition of graveyard panel

--- src/ooga/view/ui/gameplaypanel/GraveyardPanel.java
+++ src/ooga/view/ui/gameplaypanel/GraveyardPanel.java
@@ -8,8 +8,10 @@ import javafx.scene.text.Text;
 import ooga.view.ui.SharedUIComponents;
 
 public class GraveyardPanel extends SharedUIComponents {
-  public GraveyardPanel(){
+  private VBox myGraveyardContent;
 
+  public GraveyardPanel(){
+    myGraveyardContent = new VBox();
   }
 
   public Node createGraveyardPanel(){
@@ -25,25 +27,25 @@ public class GraveyardPanel extends SharedUIComponents {
   public Node createHistoryPanel() {
     VBox myHistoryPanel = new VBox();
     myHistoryPanel.setSpacing(getInt("gameplay_subpanel_spacing"));
-    myHistoryPanel.setId("history-panel");
+    myHistoryPanel.setId("graveyard-panel");
 
-    Text historyPanelTitle = makeText(getWord("history_panel_title"));
-    ScrollPane historyScrollPane = makeHistoryScrollPane();
-    myHistoryPanel.getChildren().addAll(historyPanelTitle, historyScrollPane);
+    Text graveyardPanelTitle = makeText(getWord("graveyard_panel_title"));
+    ScrollPane graveyardScrollPane = makeGraveyardScrollPane();
+    myHistoryPanel.getChildren().addAll(graveyardPanelTitle, graveyardScrollPane);
 
-    myHistoryPanel.setVgrow(historyScrollPane, Priority.ALWAYS);
+    myHistoryPanel.setVgrow(graveyardScrollPane, Priority.ALWAYS);
 
     return myHistoryPanel;
   }
 
-  private ScrollPane makeHistoryScrollPane(){
+  private ScrollPane makeGraveyardScrollPane(){
     ScrollPane newScrollPane = new ScrollPane();
     newScrollPane.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);
     newScrollPane.fitToWidthProperty().set(true);
 
-    newScrollPane.setVmax(getInt("max_history_scrollpane_height"));
-    newScrollPane.setPrefSize(getInt("pref_history_scrollpane_width"), getInt("pref_history_scrollpane_height"));
-    newScrollPane.setContent(myHistoryContent);
+    newScrollPane.setVmax(getInt("max_graveyard_scrollpane_height"));
+    newScrollPane.setPrefSize(getInt("pref_graveyard_scrollpane_width"), getInt("pref_graveyard_scrollpane_height"));
+    newScrollPane.setContent(myGraveyardContent);
 
 //    sp.vvalueProperty().addListener((ObservableValue<? extends Number> ov,
 //        Number old_val, Number new_val) -> {
@@ -54,8 +56,8 @@ public class GraveyardPanel extends SharedUIComponents {
   }
 
   public void addHistory(String action){
-    Text newHistoryEntry = makeText(action);
-    myHistoryContent.getChildren().add(newHistoryEntry);
+    Text newGraveyardEntry = makeText(action);
+    myGraveyardContent.getChildren().add(newGraveyardEntry);
   }
 
 }
