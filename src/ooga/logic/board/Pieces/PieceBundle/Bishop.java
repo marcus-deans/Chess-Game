7be1392a1cc47d18a8199199a752bd618215396d
Amr Tagel-Din <aht14@duke.edu>
2021-11-18 18:03:28 -0500

    created BishopResourceBundles and connectedit

--- src/ooga/logic/board/Pieces/PieceBundle/Bishop.java
+++ src/ooga/logic/board/Pieces/PieceBundle/Bishop.java
@@ -1,14 +1,52 @@
 package ooga.logic.board.Pieces.PieceBundle;
 
+import java.util.Map;
+import java.util.ResourceBundle;
 import ooga.logic.board.Pieces.SpotCollection.BishopMovement;
+import ooga.logic.board.Pieces.SpotCollection.SpotCollection;
 import ooga.logic.board.coordinate.GameCoordinate;
 
 public class Bishop extends Piece {
+
+  private ResourceBundle PieceProperties;
+  private static final String PIECES_PACKAGE = Bishop.class.getPackageName() + ".resources.";
+  private static final String PIECE_TO_STRING = "Bishop";
+  private static final String PIECE_PATH="ooga.logic.board.Pieces.PieceBundle.";
+
+
   public Bishop(int xPosition, int yPosition, int team){
+    PieceProperties=ResourceBundle.getBundle(PIECES_PACKAGE+PIECE_TO_STRING);
+
     setTeam(team);
     setCoordinate(new GameCoordinate(xPosition,yPosition));
-    setMyMovement(new BishopMovement());
-    setMyCapture(new BishopMovement());
+    setMovement();
+    setJump();
+  }
+
+  private void setJump() {
+    try{
+      setMyMovement(
+          (SpotCollection) Class.forName(
+                  String.format("ooga.logic.board.Pieces.PieceBundle.resources.%s", PIECE_TO_STRING))
+              .getConstructor().newInstance()
+      );
+    }
+    catch (Exception e){
+      setMyMovement(new BishopMovement());
+    }
+  }
+
+  private void setMovement() {
+    try{
+      setMyMovement(
+          (SpotCollection) Class.forName(
+                  String.format("ooga.logic.board.Pieces.PieceBundle.resources.%s", PIECE_TO_STRING))
+              .getConstructor().newInstance()
+      );
+    }
+    catch (Exception e){
+      setMyMovement(new BishopMovement());
+    }
   }
 
 
