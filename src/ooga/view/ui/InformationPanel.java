89025a8d060e5865fd92120c910e3aec1aecabd8
Marcus Deans <marcus.deans@duke.edu>
2021-11-21 22:48:19 -0500

    Added new information panel class to display welcome panel and general game information to user

---
+++ src/ooga/view/ui/InformationPanel.java
@@ -0,0 +1,16 @@
+package ooga.view.ui;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+
+public class InformationPanel extends SharedUIComponents{
+  public InformationPanel(){
+
+  }
+
+  public Node createInformationPanel(){
+    Group informationPanel = new Group();
+    return informationPanel;
+  }
+
+}
