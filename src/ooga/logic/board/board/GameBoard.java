e2d4f898755cf6ab9a5f58848d8ed932d1935b72
Jeremy Cross <jsc659@duke.edu>
2021-11-30 14:24:50 -0500

    Added edge policy filtering

--- src/ooga/logic/board/board/GameBoard.java
+++ src/ooga/logic/board/board/GameBoard.java
@@ -4,6 +4,7 @@ package ooga.logic.board.board;
 import ooga.logic.board.Pieces.PieceBundle.Piece;
 import ooga.logic.board.coordinate.Coordinate;
 import ooga.logic.board.coordinate.GameCoordinate;
+import ooga.logic.board.edgepolicies.EdgePolicies;
 import ooga.logic.board.spot.GameSpot;
 import ooga.logic.board.spot.Spot;
 
@@ -20,9 +21,12 @@ public class GameBoard implements Board {
     private int team;
     private static final String PIECES_PACKAGE =
             GameBoard.class.getPackageName() + ".resources.";
-    private static final String STRING_TO_PIECE_MAP = "PieceMap";
-    private ResourceBundle pieceMap;
+    private static final String GAMEBOARD_MAP = "GameBoard";
+    private ResourceBundle resourceMap;
     private static final String PIECE_PATH="ooga.logic.board.Pieces.PieceBundle.";
+    private static final String EDGEPOLICY_PATH="ooga.logic.board.edgepolicies.";
+    private EdgePolicies edgePolicy;
+    private String edgeName;
 
 
     public GameBoard(int rows, int columns)
@@ -30,7 +34,7 @@ public class GameBoard implements Board {
         board=new ArrayList<>();
         this.rows=rows;
         this.columns=columns;
-        pieceMap=ResourceBundle.getBundle(PIECES_PACKAGE+STRING_TO_PIECE_MAP);
+        resourceMap=ResourceBundle.getBundle(PIECES_PACKAGE+GAMEBOARD_MAP);
     }
 
     @Override
@@ -41,7 +45,7 @@ public class GameBoard implements Board {
         {
             for(int j=0;j<columns;j++)
             {
-                pieceName=PIECE_PATH+pieceMap.getString(setup[i][j].substring(0,1));
+                pieceName=PIECE_PATH+resourceMap.getString(setup[i][j].substring(0,1));
                 Class[] params={int.class,int.class,int.class};
                 team=Integer.parseInt(setup[i][j].substring(1,2));
                 Piece p;
@@ -99,5 +103,15 @@ public class GameBoard implements Board {
             }
         }
         return null;
+    }
+
+    public void setEdgePolicy(String edge) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class[] params={int.class,int.class};
+        edgeName=EDGEPOLICY_PATH+resourceMap.getString(edge);
+        edgePolicy=(EdgePolicies) Class.forName(edgeName).getDeclaredConstructor(params).newInstance(rows,columns);
+    }
+
+    public EdgePolicies getEdgePolicy() {
+        return edgePolicy;
     }
 }
