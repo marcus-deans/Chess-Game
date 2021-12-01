b4fd02bc0d15ef75c6d06835b16432f6d17c87e2
Amr Tagel-Din <aht14@duke.edu>
2021-12-01 16:07:43 -0500

    Knight and King Tests

--- test/ooga/logic/board/Pieces/PieceBundle/KnightTest.java
+++ test/ooga/logic/board/Pieces/PieceBundle/KnightTest.java
@@ -9,13 +9,13 @@ import ooga.logic.board.coordinate.GameCoordinate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class KingTest {
+class KnightTest {
   private Piece myPiece;
   private Coordinate myCoord;
   @BeforeEach
   void setUp() {
-    myPiece = new King(0,2,1);
-    myCoord = new GameCoordinate(0,2);
+    myPiece = new Knight(2,2,1);
+    myCoord = new GameCoordinate(2,2);
   }
 
   @Test
@@ -32,15 +32,15 @@ class KingTest {
 
   @Test
   void canCapture() {
-    myCoord.setCoordinate(0,3);
+    myCoord.setCoordinate(3,4);
     assertTrue(myPiece.canCapture(myCoord));
   }
 
   @Test
   void getMyMovement() {
-    myCoord.setCoordinate(2,4);
+    myCoord.setCoordinate(2,3);
     assertFalse(myPiece.getPossibleMoves().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
-    myCoord.setCoordinate(0,3);
+    myCoord.setCoordinate(1,0);
     assertTrue(myPiece.getPossibleMoves().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
   }
 
@@ -51,6 +51,7 @@ class KingTest {
     myCoord.setCoordinate(0,1);
     assertTrue(myPiece.getPossibleCaptures().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
   }
+
 
   @Test
   void setAndGetTeam() {
