4e2d324885d7f1804932bae0a88514f5511b6f0f
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2021-11-30 19:17:23 -0500

    Added player control panel class

---
+++ src/ooga/view/ui/controlpanel/PlayerControlPanel.java
@@ -0,0 +1,9 @@
+package ooga.view.ui.controlpanel;
+
+import ooga.view.ui.SharedUIComponents;
+
+public class PlayerControlPanel extends SharedUIComponents {
+  public PlayerControlPanel(){
+
+  }
+}
