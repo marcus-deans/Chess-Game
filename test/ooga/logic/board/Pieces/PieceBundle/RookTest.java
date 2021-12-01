a6402de55a763e6eaa674d79593e3f94790462cf
Amr Tagel-Din <aht14@duke.edu>
2021-12-01 16:10:47 -0500

    Knight Tests added

--- test/ooga/logic/board/Pieces/PieceBundle/RookTest.java
+++ test/ooga/logic/board/Pieces/PieceBundle/RookTest.java
@@ -7,66 +7,35 @@ import ooga.logic.board.coordinate.GameCoordinate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class KnightTest {
+class RookTest {
   private Piece myPiece;
   private Coordinate myCoord;
   @BeforeEach
   void setUp() {
-    myPiece = new Knight(2,2,1);
+    myPiece = new Rook(2,2,1);
     myCoord = new GameCoordinate(2,2);
   }
 
   @Test
-  void getCoordinate() {
-    assertEquals(myPiece.getCoordinate(),myCoord);
-  }
-
-  @Test
-  void setCoordinate() {
-    myCoord.setCoordinate(2,4);
-    myPiece.setCoordinate(myCoord);
-    assertEquals(myPiece.getCoordinate(),myCoord);
-  }
-
-  @Test
   void canCapture() {
-    myCoord.setCoordinate(3,4);
+    myCoord.setCoordinate(2,4);
     assertTrue(myPiece.canCapture(myCoord));
   }
 
   @Test
   void getMyMovement() {
-    myCoord.setCoordinate(2,3);
+    myCoord.setCoordinate(3,3);
     assertFalse(myPiece.getPossibleMoves().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
-    myCoord.setCoordinate(1,0);
+    myCoord.setCoordinate(2,0);
     assertTrue(myPiece.getPossibleMoves().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
   }
 
   @Test
   void getMyCapture() {
-    myCoord.setCoordinate(2,4);
+    myCoord.setCoordinate(4,4);
     assertFalse(myPiece.getPossibleCaptures().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
-    myCoord.setCoordinate(0,1);
+    myCoord.setCoordinate(7,2);
     assertTrue(myPiece.getPossibleCaptures().getPossibleSpots(myPiece.getCoordinate()).contains(myCoord));
-  }
-
-
-  @Test
-  void setAndGetTeam() {
-    myPiece.setTeam(3);
-    assertEquals(myPiece.getTeam(),3);
-  }
-
-  @Test
-  void setAndGetValue() {
-    myPiece.setValue(9);
-    assertEquals(myPiece.getValue(),9);
-  }
-
-  @Test
-  void setAndGetCanJump() {
-    myPiece.setCanJump(true);
-    assertTrue(myPiece.getCanJump());
   }
 
 }
