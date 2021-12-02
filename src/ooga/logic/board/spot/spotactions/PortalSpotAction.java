137764dd07b0c36c7caa65440393ddccbcb48e29
Jeremy Cross <jsc659@duke.edu>
2021-12-02 00:21:08 -0500

    Debugged blackhole and portal spots and added loggers

--- src/ooga/logic/board/spot/spotactions/PortalSpotAction.java
+++ src/ooga/logic/board/spot/spotactions/PortalSpotAction.java
@@ -6,10 +6,13 @@ import ooga.logic.board.spot.Spot;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PortalSpotAction implements SpotAction{
-    private boolean search=false;
-    private boolean search2=false;
+    private boolean search=true;
+    private boolean search2=true;
+    private Logger myLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     @Override
     public List<Spot> commitSpotAction(List<Spot> a, Spot s) {
@@ -30,9 +33,10 @@ public class PortalSpotAction implements SpotAction{
             if (a.get(random).getPiece()==null)
             {
                 ((GameSpot) a.get(random)).setTypeOfSpot(1);
-                search2=true;
+                search2=false;
             }
         }
+        myLogger.log(Level.INFO, "Your piece has entered a portal and teleported to a random spot");
         return a;
     }
 }
