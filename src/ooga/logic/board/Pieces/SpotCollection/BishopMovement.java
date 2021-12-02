9a9585d8b7ad2eb727832791ba18e92dd60850dc
Amr Tagel-Din <aht14@duke.edu>
2021-12-02 17:02:30 -0500

    bishop movement super

--- src/ooga/logic/board/Pieces/SpotCollection/BishopMovement.java
+++ src/ooga/logic/board/Pieces/SpotCollection/BishopMovement.java
@@ -6,6 +6,10 @@ import ooga.logic.board.coordinate.Coordinate;
 public class BishopMovement extends SpotCollection {
   private static final String PIECE_AS_STRING = "bishop";
 
+  public BishopMovement(){
+    super();
+  }
+
   @Override
   public List<Coordinate> getPossibleSpots(Coordinate myCoordinate) {
     return ContinuousPossibleSpots(PIECE_AS_STRING, myCoordinate);
