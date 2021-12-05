4a1621c40ee3937d17a9d35b6e999682a0148938
Amr Tagel-Din <aht14@duke.edu>
2021-12-05 16:49:07 -0500

    updated BasicEdgePolicy stream

--- src/ooga/logic/board/edgepolicies/BasicEdgePolicies.java
+++ src/ooga/logic/board/edgepolicies/BasicEdgePolicies.java
@@ -22,21 +22,13 @@ public class BasicEdgePolicies implements EdgePolicies{
 
         List<List<Coordinate>> possibleMoves= new ArrayList<>();
         for (List<Coordinate> miniList : allMoves){
-            List<Coordinate> filteredList = new ArrayList<>();
-            miniList.stream()
+            List<Coordinate> filteredList = miniList.stream()
                 .filter(move -> move.getX_pos()<width && move.getX_pos()>=0 && move.getY_pos()<height&& move.getY_pos()>=0)
-                .collect(Collectors.toCollection(() -> filteredList));
+                .collect(Collectors.toList());
             possibleMoves.add(filteredList);
 
         }
 
-
-//        List<Coordinate> possibleMoves = new ArrayList<>();
-//        for (Coordinate c: allMoves) {
-//            if(c.getX_pos()<width&&c.getX_pos()>=0&&c.getY_pos()<height&&c.getY_pos()>=0) {
-//                possibleMoves.add(c);
-//            }
-//        }
         return possibleMoves;
     }
 }
