2f1603f45af49fa6892b53f45b6717917e725709
Amr Tagel-Din <aht14@duke.edu>
2021-12-05 05:32:30 -0500

    fixed Bishop, Knight, King, and Pawn tests

--- test/ooga/logic/board/Pieces/SpotCollection/KnightMovementTest.java
+++ test/ooga/logic/board/Pieces/SpotCollection/KnightMovementTest.java
@@ -1,50 +1,58 @@
-//package ooga.logic.board.Pieces.SpotCollection;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import java.util.List;
-//import ooga.logic.board.Pieces.SpotCollection.SpecificSpotCollectionBundle.ContinuousLine;
-//import ooga.logic.board.Pieces.SpotCollection.SpecificSpotCollectionBundle.SpecificSpotCollection;
-//import ooga.logic.board.coordinate.Coordinate;
-//import ooga.logic.board.coordinate.GameCoordinate;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//class KnightMovementTest {
-//  private SpotCollection mySpotCollection;
-//  private Coordinate innerCoordinate;
-//  private Coordinate outerCoordinate;
-//  private Coordinate topCoordinate;
-//
-//  @BeforeEach
-//  void setUp() {
-//    mySpotCollection = new KnightMovement();
-//    innerCoordinate = new GameCoordinate(2,2);
-//    outerCoordinate = new GameCoordinate(0,0);
-//    topCoordinate = new GameCoordinate(3,7);
-//  }
-//
-//  @Test
-//  void getPossibleSpotsInnerCoordinate() {
-//    List<Coordinate> myCoords = mySpotCollection.getPossibleSpots(innerCoordinate);
-////    Coordinate myCoord1 = new GameCoordinate(1,3);
-////    Coordinate myCoord2 = new GameCoordinate(3,3);
-//    assertTrue(myCoords.size() == 8);
-//  }
-//
-//  @Test
-//  void getPossibleSpotsOuterCoordinate() {
-//    List<Coordinate> myCoords = mySpotCollection.getPossibleSpots(outerCoordinate);
-////    Coordinate myCoord1 = new GameCoordinate(1,3);
-////    Coordinate myCoord2 = new GameCoordinate(3,3);
-//    assertTrue(myCoords.size() == 2);
-//  }
-//
-//  @Test
-//  void getPossibleSpotsTopCoordinate() {
-//    List<Coordinate> myCoords = mySpotCollection.getPossibleSpots(topCoordinate);
-////    Coordinate myCoord1 = new GameCoordinate(1,3);
-////    Coordinate myCoord2 = new GameCoordinate(3,3);
-//    assertTrue(myCoords.size() == 4);
-//  }
-//}
+package ooga.logic.board.Pieces.SpotCollection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import ooga.logic.board.Pieces.SpotCollection.SpecificSpotCollectionBundle.ContinuousLine;
+import ooga.logic.board.Pieces.SpotCollection.SpecificSpotCollectionBundle.SpecificSpotCollection;
+import ooga.logic.board.coordinate.Coordinate;
+import ooga.logic.board.coordinate.GameCoordinate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class KnightMovementTest {
+  private SpotCollection mySpotCollection;
+  private Coordinate innerCoordinate;
+  private Coordinate outerCoordinate;
+  private Coordinate topCoordinate;
+
+  @BeforeEach
+  void setUp() {
+    mySpotCollection = new KnightMovement();
+    innerCoordinate = new GameCoordinate(2,2);
+    outerCoordinate = new GameCoordinate(0,0);
+    topCoordinate = new GameCoordinate(3,7);
+  }
+
+  @Test
+  void getPossibleSpotsInnerCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(innerCoordinate);
+    int size = getSize(myCoords);
+    assertTrue(size == 8);
+  }
+
+
+
+  @Test
+  void getPossibleSpotsOuterCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(outerCoordinate);
+
+    int size = getSize(myCoords);
+    assertTrue(size == 2);
+  }
+
+  @Test
+  void getPossibleSpotsTopCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(topCoordinate);
+    int size = getSize(myCoords);
+    assertTrue(size == 4);
+  }
+
+  private int getSize(List<List<Coordinate>> myCoords) {
+    int size = 0;
+    for (List<Coordinate> x : myCoords){
+      size += x.size();
+    }
+    return size;
+  }
+}
