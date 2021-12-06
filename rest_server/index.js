c4855687c1a9e18448727d81fda90a428c2abda9
Tim Jang <sj308@duke.edu>
2021-12-06 13:20:26 -0500

    updated rest server fully

--- rest_server/index.js
+++ rest_server/index.js
@@ -1,7 +1,8 @@
+
 let mongoose = require('mongoose');
 
 mongoose
-	.connect("mongodb+srv://cs307:robertduvall@cluster0.6eljb.mongodb.net/cs307Chess?retryWrites=true&w=majority")
+    .connect("mongodb+srv://cs307:robertduvall@cluster0.6eljb.mongodb.net/cs307Chess?retryWrites=true&w=majority")
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -17,14 +18,14 @@ let userSchema = new mongoose.Schema({
     profileColor: {type: String, required: false},
     wins: {type: Number, required: false},
     losses: {type: Number, required: false}
-  })
+})
 
 // defining the Express app
 const app = express();
 
 // defining an array to work as the database (temporary solution)
 const ads = [
-  {title: 'Hello, world (again)!'}
+    {title: 'Hello, world (again)!'}
 ];
 
 // adding Helmet to enhance your API's security
@@ -41,8 +42,14 @@ app.use(morgan('combined'));
 
 // defining an endpoint to return all ads
 app.get('/', (req, res) => {
-  res.send(ads);
+    res.send(ads);
 });
+
+async function findUser(Users, query){
+    let resp = await Users.findOne(query);
+
+    return resp;
+}
 
 app.get('/createUser', (req, res) => {
     let id = req.query.id
@@ -50,30 +57,152 @@ app.get('/createUser', (req, res) => {
 
     // id = "test1"
     // password = "test"
-
     console.log(id)
     console.log(password)
 
+    let query = {
+        _id: id
+    }
+
     let collectionName = "userInfo"
-    
+
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")   
+    res.header("Access-Control-Allow-Headers", "*")
+
+    const Users = mongoose.model(collectionName, userSchema, collectionName)
+
+    findUser(Users, query).then(r => {
+        console.log(r)
+
+        if(!r) {
+            Users.create({
+                _id: id,
+                password: password,
+                profileColor:"",
+                wins:0,
+                losses:0
+            })
+            res.send("createduser")
+        }
+        else if(r.password !== password){
+            res.send("wrongpassword")
+        }else{
+            res.send("loggedin")
+        }
+    })
+});
+
+app.get('/setProfileColor', (req, res) => {
+    let id = req.query.id
+    let color = req.query.color
+
+    console.log(id)
+
+    let query = {
+        _id: id
+    }
+
+    let collectionName = "userInfo"
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
 
     const Users = mongoose.model(collectionName, userSchema, collectionName)
 
     Users.findOneAndUpdate(
-        {
-            _id: id,
-            password:password,
-            profileColor:"",
-            wins:0,
-            losses:0
-        }
-    )
-        res.send("success????")
-  });
+        query,
+        {profileColor: color},
+        { upsert: true, new: true, setDefaultsOnInsert: true },
+        function(error, result) {
+            if (error){
+                res.send("error")
+                return;
+            }
+            res.send("updated")
+        })
+});
+
+app.get('/getUserScore', (req, res) => {
+    let id = req.query.id
+
+    console.log(id)
+
+    let query = {
+        _id: id
+    }
+
+    let collectionName = "userInfo"
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+
+    const Users = mongoose.model(collectionName, userSchema, collectionName)
+
+    Users.findById(id).then( r=> {
+        res.send(r.wins + ", " + r.losses);
+    })
+});
+
+
+app.get('/addScore', (req, res) => {
+    let id = req.query.id
+
+    console.log(id)
+
+    let query = {
+        _id: id
+    }
+
+    let collectionName = "userInfo"
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+
+    const Users = mongoose.model(collectionName, userSchema, collectionName)
+
+    Users.findOneAndUpdate(
+        query,
+        {$inc : {wins : 1}},
+        { upsert: true, new: true, setDefaultsOnInsert: true },
+        function(error, result) {
+            if (error){
+                res.send("error")
+                return;
+            }
+            res.send("updated")
+        })
+});
+
+app.get('/subtractScore', (req, res) => {
+    let id = req.query.id
+
+    console.log(id)
+
+    let query = {
+        _id: id
+    }
+
+    let collectionName = "userInfo"
+
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+
+    const Users = mongoose.model(collectionName, userSchema, collectionName)
+
+    Users.findOneAndUpdate(
+        query,
+        {$inc : {losses : 1}},
+        { upsert: true, new: true, setDefaultsOnInsert: true },
+        function(error, result) {
+            if (error){
+                res.send("error")
+                return;
+            }
+            res.send("updated")
+        })
+});
 
 // starting the server
 app.listen(3001, () => {
-  console.log('listening on port 3001');
+    console.log('listening on port 3001');
 });
