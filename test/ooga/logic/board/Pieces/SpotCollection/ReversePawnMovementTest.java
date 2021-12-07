09e0731ac3ff7500d8d5bc3115f624277f2246ce
Amr Tagel-Din <aht14@duke.edu>
2021-12-06 21:34:52 -0500

    added reverse movement and captures

---
+++ test/ooga/logic/board/Pieces/SpotCollection/ReversePawnMovementTest.java
@@ -0,0 +1,56 @@
+package ooga.logic.board.Pieces.SpotCollection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import ooga.logic.board.coordinate.Coordinate;
+import ooga.logic.board.coordinate.GameCoordinate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReversePawnMovementTest {
+  private SpotCollection mySpotCollection;
+  private Coordinate innerCoordinate;
+  private Coordinate outerCoordinate;
+  private Coordinate topCoordinate;
+
+  @BeforeEach
+  void setUp() {
+    mySpotCollection = new ReversePawnMovement();
+    innerCoordinate = new GameCoordinate(2,2);
+    outerCoordinate = new GameCoordinate(0,0);
+    topCoordinate = new GameCoordinate(3,6);
+  }
+
+  @Test
+  void getPossibleSpotsInnerCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(innerCoordinate);
+    int size = getSize(myCoords);
+    assertTrue(size == 1);
+  }
+
+
+
+  @Test
+  void getPossibleSpotsOuterCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(outerCoordinate);
+
+    int size = getSize(myCoords);
+    assertTrue(size == 1);
+  }
+
+  @Test
+  void getPossibleSpotsTopCoordinate() {
+    List<List<Coordinate>> myCoords = mySpotCollection.getPossibleSpots(topCoordinate);
+    int size = getSize(myCoords);
+    assertTrue(size == 2);
+  }
+
+  private int getSize(List<List<Coordinate>> myCoords) {
+    int size = 0;
+    for (List<Coordinate> x : myCoords){
+      size += x.size();
+    }
+    return size;
+  }
+}
