2ced711f5560426e035dd299c97223d56b20f1ee
Tim Jang <sj308@duke.edu>
2021-12-07 02:57:37 -0500

    javadoc for game

--- src/ooga/logic/game/Player.java
+++ src/ooga/logic/game/Player.java
@@ -35,11 +35,18 @@ public class Player {
         myLogger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     }
 
+    /**
+     * method for reading from the properties file given a key
+     * @param key
+     */
     private String readFromProperties(String key){
          return myResource.getString(key);
     }
 
-
+    /**
+     * calls get request to the given URL. This is how we recieve and send data to the database.
+     * @param givenURL
+     */
     private String getFromDatabase(String givenURL) throws IOException {
         URL url = new URL(givenURL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -54,7 +61,10 @@ public class Player {
         return result.toString();
     }
 
-
+    /**
+     * This method checks whether the user has successfully signed in or not. If not, they need to re-enter their
+     * password (returns false)
+     */
     public boolean checkUser() throws IOException {
         String url = apiURL + readFromProperties("check_user_path") + "?" + readFromProperties("id_parameter") + myUsername + "&" + readFromProperties("password_parameter") + myPassword;
 
@@ -66,6 +76,12 @@ public class Player {
         return false;
     }
 
+    /**
+     * This method updates the win and the loss count for each user in the database. This means even when you close
+     * the program fully locally, it is still saved in the cloud and can retrieve it.
+     *
+     * @param didWin
+     */
     public void updateUserScore(boolean didWin) throws IOException {
         String result;
         if(didWin){
@@ -77,36 +93,69 @@ public class Player {
         myLogger.log(Level.INFO, result);
     }
 
+    /**
+     * This method sets profile color and saves it in the database for this player
+     *
+     * @param color
+     */
     public void setProfileColor(String color) throws IOException {
         String result = getFromDatabase(apiURL + readFromProperties("set_profile_color_path") + "?" + readFromProperties("id_parameter") + myUsername+ "&color=" + color);
 
         myLogger.log(Level.INFO, result);
     }
 
+    /**
+     * This method gets the profile color of this player from the database
+     *
+     * @return String
+     */
     public String getProfileColor() throws IOException {
         String result = getFromDatabase(apiURL + readFromProperties("get_profile_color_path") + "?" + readFromProperties("id_parameter") + myUsername);
 
         return result;
     }
 
+    /**
+     * This method gets the score of this player from the database. returns both wincount and losscount.
+     *
+     * @return String
+     */
     public String getUserScore() throws IOException {
         String result = getFromDatabase(apiURL + readFromProperties("get_user_score_path") + "?" + readFromProperties("id_parameter") + myUsername);
 
         return result;
     }
 
+    /**
+     * returns the username of this player.
+     *
+     * @return String
+     */
     public String getUsername() {
         return myUsername;
     }
 
+    /**
+     * returns the team of this player.
+     *
+     * @return int
+     */
     public int getTeam(){
         return myTeam;
     }
 
+    /**
+     * adds piece to the player's graveyard if their piece was captured.
+     *
+     * @param piece
+     */
     public void addPieceToGraveyard(Piece piece){
         graveyard.add(piece);
     }
 
+    /**
+     * returns player's graveyard.
+     */
     public List<Piece> getGraveyard(){
         return graveyard;
     }
