04a3b229bc46b32bad6b65eb7ec8b1ed5fb3ad33
Amr Tagel-Din <aht14@duke.edu>
2021-12-07 01:07:33 -0500

    added comments for all the booleanStorage subclasses

--- src/ooga/logic/board/Pieces/PieceBundle/BooleanStorageClasses/cannibalizeStorage.java
+++ src/ooga/logic/board/Pieces/PieceBundle/BooleanStorageClasses/cannibalizeStorage.java
@@ -11,7 +11,13 @@ import java.util.ResourceBundle;
 public class cannibalizeStorage extends BooleanStorage {
   private static final String MY_STRING = "canCannibalize";
 
-
+  /**
+   * Pass properties back to calculate the cannibalization property for this piece
+   * @param attributeMap Added Map of properties that will give the pieces quick flexibility
+   * @param pieceProperties Properties of the specific piece we have chosen
+   * @param defaultProperties Default properties for a piece if the pieces hasn't been given
+   *                          a property
+   */
   public cannibalizeStorage(Map<String, String> attributeMap, ResourceBundle pieceProperties, ResourceBundle defaultProperties) {
     super(MY_STRING, attributeMap,pieceProperties,defaultProperties);
   }
--- src/ooga/logic/board/Pieces/PieceBundle/BooleanStorageClasses/checkableStorage.java
+++ src/ooga/logic/board/Pieces/PieceBundle/BooleanStorageClasses/checkableStorage.java
@@ -11,6 +11,13 @@ import java.util.ResourceBundle;
 public class checkableStorage extends BooleanStorage {
   private static final String MY_STRING = "isCheckable";
 
+  /**
+   * Pass properties back to calculate the checkable property for this piece
+   * @param attributeMap Added Map of properties that will give the pieces quick flexibility
+   * @param pieceProperties Properties of the specific piece we have chosen
+   * @param defaultProperties Default properties for a piece if the pieces hasn't been given
+   *                          a property
+   */
   public checkableStorage(Map<String, String> attributeMap, ResourceBundle pieceProperties, ResourceBundle defaultProperties) {
     super(MY_STRING, attributeMap,pieceProperties,defaultProperties);
   }
